c927d01a718409928fee775a41b977463bc3e974
Nate Sanden <natesanden@gmail.com>
2016-01-31 12:31:46 -0700

    path has changed

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -6,7 +6,7 @@ $kernel->init([
     'debug' => true,
     'includePaths' => [__DIR__.'/../common'],
 ]);
-$kernel->loadFile(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
+$kernel->loadFile(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = yii\helpers\ArrayHelper::merge(
 	require(__DIR__ . '/../frontend/config/main.php'),
